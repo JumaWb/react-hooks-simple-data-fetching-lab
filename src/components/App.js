ed10ee68cd40d4ddba8b30a0410f0a23908fedee
Brian Juma <bjuma0985@gmail.com>
2024-05-01 22:57:33 +0300

    react-data-fetching-lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,39 @@
 // create your App component here
+import { useEffect, useState } from "react";
+
+//create an app function
+function App(){
+    const [dogImage, setDogImage]=useState(null);
+    const[isLoading, setIsLoading]=useState(true);
+
+    useEffect(()=>{
+        //fetching random image
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(response=>{
+            if(!response.ok){
+                throw new Error("Failed to Fetch Dog Image");
+            }
+            return response.json();
+        })
+        .then(data=>{
+            //Update the dog image in URL
+            setDogImage(data.message);
+            setIsLoading(false);
+        })
+        .catch(error =>{
+            console.error("Error fetching dog image:");
+
+        })
+    }, []);
+    return(
+        <div>
+            {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        // Show dog image once fetched
+        <img src={dogImage} alt="A Random Dog" />
+      )}
+        </div>
+    )
+}
+export default App;
